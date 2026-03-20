04a4cac642f5dd3b1bd52df71c41ff65943d9f40
sam <samt@samt.st>
2026-03-20 14:04:16 +0700

    remove nostr band from defaults

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,6 @@ const persistOptions = {
 const DEFAULT_DISCOVERY_RELAYS: string[] = [
   'wss://relay.ditto.pub',
   'wss://relay.damus.io',
-  'wss://relay.nostr.band',
   'wss://relay.primal.net',
   'wss://nos.lol',
 ];
